b491b2a758085208b3cceec0f04de249357f4e91
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-05-26 16:38:27 -0300

    DB

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -4,10 +4,14 @@ import 'firebase/compat/firestore';
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        apiKey: 'AIzaSyCitZndUOJRmS2rwdad9q_EF7urDlMecx0',
+        authDomain: 'cadastro-fullstack.firebaseapp.com',
+        projectId: 'cadastro-fullstack',
     })
 }
 
 export default firebase
+
+// apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
